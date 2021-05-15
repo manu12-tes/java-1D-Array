4515e54f18f3eedbfb06bdd1f39687f9b391906a
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-14 23:44:42 -0500

    Agregar archivos de proyecto.

---
+++ java-1D-Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace java_1D_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero de enteros");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] arreglo = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arreglo[i] = Int32.Parse(Console.ReadLine());
+
+            for (int j = 0; j < arreglo.Length; j++)
+                Console.WriteLine(arreglo[j]);
+
+        }
+    }
+}
